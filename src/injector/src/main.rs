995134a9f5a95533e73a25467e5395e303c875d9
Lucas Rodriguez <lucas.rodriguez@defenseunicorns.com>
2024-05-14 13:14:29 -0500

    Return content-length header

--- src/injector/src/main.rs
+++ src/injector/src/main.rs
@@ -196,11 +196,24 @@ async fn handle_get_manifest(name: String, reference: String) -> Response {
             .join(&sha_manifest);
         match tokio::fs::File::open(&file_path).await {
             Ok(file) => {
+                let metadata = match file.metadata().await {
+                    Ok(meta) => meta,
+                    Err(_) => {
+                        return Response::builder()
+                            .status(StatusCode::INTERNAL_SERVER_ERROR)
+                            .body("Failed to get file metadata".into())
+                            .unwrap()
+                    }
+                };
                 let stream = ReaderStream::new(file);
                 Response::builder()
                     .status(StatusCode::OK)
                     .header("Content-Type", OCI_MIME_TYPE)
-                    .header("Docker-Content-Digest", sha_manifest.clone())
+                    .header("Content-Length", metadata.len())
+                    .header(
+                        "Docker-Content-Digest",
+                        format!("sha256:{}", sha_manifest.clone()),
+                    )
                     .header("Etag", format!("sha256:{}", sha_manifest))
                     .header("Docker-Distribution-Api-Version", "registry/2.0")
                     .body(Body::from_stream(stream))
